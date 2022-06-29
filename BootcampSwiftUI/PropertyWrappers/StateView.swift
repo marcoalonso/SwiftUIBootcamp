f7be9f867813d045e5ef167bffcf7f266b76b3c3
Marco Alonso <marco@garagecoders.net>
2022-06-28 23:47:27 -0500

    State & Binding
    Property Wrappers Added

---
+++ BootcampSwiftUI/PropertyWrappers/StateView.swift
@@ -0,0 +1,20 @@
+//
+//  StateView.swift
+//  BootcampSwiftUI
+//
+//  Created by marco rodriguez on 28/06/22.
+//
+
+import SwiftUI
+
+struct StateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StateView_Previews: PreviewProvider {
+    static var previews: some View {
+        StateView()
+    }
+}
