478f9f22fe13cb45d26d82456efb27155daca10f
Marco Alonso <marco@garagecoders.net>
2022-06-28 23:26:39 -0500

    Programadores favoritos
    agregados

--- BootcampSwiftUI/Lists/ListDetailView.swift
+++ BootcampSwiftUI/Lists/ListDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ListDetailView: View {
     
     var programmer : Programmer
+    @Binding var favorite: Bool
     
     var body: some View {
         VStack {
@@ -17,7 +18,22 @@ struct ListDetailView: View {
                 .resizable().frame(width: 200, height: 200, alignment: .center)
                 .clipShape(Circle())
                 .overlay(Circle().stroke(Color.black, lineWidth: 4)).shadow(color: Color.gray, radius: 5, x: 10, y: 0)
-            Text(programmer.name).font(.largeTitle)
+            HStack {
+                Text(programmer.name).font(.largeTitle)
+                Button {
+                    //action
+                    favorite.toggle()
+                } label: {
+                
+                    if favorite {
+                        Image(systemName: "star.fill").foregroundColor(.yellow)
+                    } else {
+                        Image(systemName: "star").foregroundColor(.blue)
+                    }
+                   
+                }
+            }
+            
             Text(programmer.languajes).font(.title)
             Spacer()
         }
@@ -26,6 +42,6 @@ struct ListDetailView: View {
 
 struct ListDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ListDetailView(programmer: Programmer(id: 1, name: "Marco", languajes: "Swift", avatar: Image(systemName: "person.fill"), favorite: true))
+        ListDetailView(programmer: Programmer(id: 1, name: "Marco", languajes: "Swift", avatar: Image(systemName: "person.fill"), favorite: true), favorite: .constant(false))
     }
 }
--- BootcampSwiftUI/Lists/ListView.swift
+++ BootcampSwiftUI/Lists/ListView.swift
@@ -7,53 +7,61 @@
 
 import SwiftUI
 
-private let programmers = [
-    Programmer(id: 1, name: "Alonso", languajes: "Objective C, Kotlin", avatar: Image(systemName: "heart"), favorite: true ),
-    Programmer(id: 2, name: "Marco", languajes: "Ruby, Swift", avatar: Image(systemName: "person.fill.checkmark"), favorite: false),
-    Programmer(id: 3, name: "Abel", languajes: "Flutter, JAVA", avatar: Image(systemName: "note"), favorite: false),
-    Programmer(id: 4, name: "Jimena", languajes: "C#, Swift", avatar: Image(systemName: "car"), favorite: true),
-    Programmer(id: 5, name: "Julio", languajes: "Python, JAVA", avatar: Image(systemName: "cloud"), favorite: true),
-    Programmer(id: 6, name: "Ricardo", languajes: "Swift, Go", avatar: Image(systemName: "person.badge.plus"), favorite: false),
-    Programmer(id: 7, name: "Juan", languajes: "Flutter, Swift", avatar: Image(systemName: "person.badge.clock"), favorite: false)
-]
+//El valor fav de un prog sea mutable opara que se vea en dif pantallas
+//crearemos un mdoelo de datos
 
+final class ProgrammersModelData: ObservableObject {
+
+@Published var programmers = [
+    Programmer(id: 0, name: "Alonso", languajes: "Objective C, Kotlin", avatar: Image(systemName: "heart"), favorite: true ),
+    Programmer(id: 1, name: "Marco", languajes: "Ruby, Swift", avatar: Image(systemName: "person.fill.checkmark"), favorite: false),
+    Programmer(id: 2, name: "Abel", languajes: "Flutter, JAVA", avatar: Image(systemName: "note"), favorite: false),
+    Programmer(id: 3, name: "Jimena", languajes: "C#, Swift", avatar: Image(systemName: "car"), favorite: true),
+    Programmer(id: 4, name: "Julio", languajes: "Python, JAVA", avatar: Image(systemName: "car.fill"), favorite: true),
+    Programmer(id: 5, name: "Ricardo", languajes: "Swift, Go", avatar: Image(systemName: "person.badge.plus"), favorite: false),
+    Programmer(id: 6, name: "Juan", languajes: "Flutter, Swift", avatar: Image(systemName: "person.badge.clock"), favorite: false),
+    Programmer(id: 7, name: "Santiago", languajes: "JAVA, C#, Swift", avatar: Image(systemName: "person.fill"), favorite: false)
+]
+}
 
 struct ListView: View {
     
     // MARK: - Properties
+    @EnvironmentObject var programmersModelData: ProgrammersModelData
+    
     @State private var showFavorites = false
     //Crearemos una variable calculada
     private var filteredProgrammers: [Programmer] {
-        return programmers.filter { programmer in
+        return programmersModelData.programmers.filter { programmer in
             return !showFavorites || programmer.favorite
         }
     }
     
     var body: some View {
         
-        VStack {
-            Toggle(isOn: $showFavorites) {
-                Text("Mostrar Favoritos")
-            }.shadow(radius: 12)
-            .padding()
+        NavigationView {
             
-            NavigationView {
+            VStack {
+                
+                Toggle(isOn: $showFavorites) {
+                    Text("Mostrar Favoritos")
+                }.shadow(radius: 12)
+                    .padding()
+                
+                
                 List (filteredProgrammers, id: \.id) { programmer in
                     
-                    NavigationLink(destination: ListDetailView(programmer: programmer)) {
+                    NavigationLink(destination: ListDetailView(programmer: programmer, favorite: $programmersModelData.programmers[programmer.id].favorite)) {
                         RowView(programmer: programmer)
                     }
-                    
-                   
                 }
-                .navigationTitle("Programadores")
-            }
+            }.navigationTitle("Programadores")
         }
     }
 }
 
 struct Listas_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        ListView().environmentObject(ProgrammersModelData())
     }
 }
